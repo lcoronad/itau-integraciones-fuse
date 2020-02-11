6169ae208f1dae2bf45e4a143a7c5e2c283ab929
Elkin Fabian Rozo Ortegon <OER00362@ITAUCO.COL>
2020-02-11 10:47:40 -0500

    Ajuste en request

---
+++ nota-credito-reverso/src/main/java/com/itau/exception/BusException.java
@@ -0,0 +1,13 @@
+package com.itau.exception;
+
+public class BusException extends Exception {
+
+	
+	private static final long serialVersionUID = -3030130514800076370L;
+	
+	public BusException() {
+		super("");
+		
+	}
+
+}
